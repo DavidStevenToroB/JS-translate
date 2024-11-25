20e84b3e97bf99e14cf335d3f674d4e42709f05c
Sena <dimatesebas73@gmail.com>
2024-11-25 17:52:34 -0500

    styles

--- script/app.js
+++ script/app.js
@@ -1,12 +1,29 @@
 import { dictionary } from "./dictionary.js"
-const storeDictionary = localStorage.getItem('storeD')
-let dictionary = JSON.parse(storeDictionary) || []
 
+const spanishWord = document.getElementById('form-Spanish')
+const englishWord = document.getElementById('form-English')
+const exampleWord = document.getElementById('form-Example') 
+const formBtn = document.getElementById('form-btn') 
 const btnTranslate = document.getElementById('translate')
 const answer = document.getElementById('answer')
 const asideDictionary =  document.getElementById('aside-Dictionary')
 const optCategories = document.getElementById('select-categories')
 const sortOption = document.getElementById('A-Z')
+const btn_adder = document.getElementById('add')
+const close = document.getElementById('close')
+
+//view
+btn_adder.addEventListener('click', (() => {
+  const adder_section = document.querySelector('.form')
+  adder_section.style.display = 'block'
+}
+))
+
+//close
+const closed = () => {
+ const adder_section = document.querySelector('.form')
+  adder_section.style.display = 'none'
+}
 
 //search
 const filterWord = () => {
@@ -259,8 +276,50 @@ const verbs = () => {
   })
 }
 
+//form
+const chooseCategorie = () => {
+  let word 
+  let categories = document.getElementById('create-word').value
+  
+  if (categories == "animals") {
+    word = dictionary.categories.animals
+    createWord(word)
+  } else if (categories === "fruits") {
+    word = dictionary.categories.fruits
+    createWord(word)
+  } else if (categories === "colors") {
+    word = dictionary.categories.colors
+    createWord(word)
+  } else if (categories === "descriptions") {
+    word = dictionary.categories.physical_descriptions
+    createWord(word)
+  } else if (categories === "skills") {
+    word = dictionary.categories.skills
+    createWord(word)
+  } else if (categories ===  "verbs") {
+    word = dictionary.categories.verbs
+    createWord(word)
+  }
+  
+}
+const createWord = (word) => {
+  let id = word.length + 1
+  let spanish = spanishWord.value
+  let english = englishWord.value
+  let example = exampleWord.value
+
+  let wordTranlation = {
+    "id": id ,"english": english, "spanish": spanish, "example": example
+  }
+  
+  word.push(wordTranlation)
+  closed()
+}
+
+formBtn.addEventListener('click', chooseCategorie)
 btnTranslate.addEventListener('click', englishOrSpanish)
 optCategories.addEventListener('change', categories)
 sortOption.addEventListener('change', categories)
+close.addEventListener('click',closed)
 
 window.addEventListener('DOMContentLoaded', categories)
